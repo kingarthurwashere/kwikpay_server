df5a3a37572f03c9c935db86ddf04b1316500979
kingarthurwashere <arthurnyasango1@outlook.com>
2022-09-07 15:59:53 +0200

    new changes

--- kwikpay/views.py
+++ kwikpay/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import Transaction, AirtimeTransaction, ZesaTransactions
 from .serializers import (
-    TransactionSerializer, 
-    AirtimeTransactionSerializer, 
+    TransactionSerializer,
+    AirtimeTransactionSerializer,
     ZesaTransactionSerializer)
 
 
@@ -11,23 +11,26 @@ class TransactionList(generics.ListCreateAPIView):
     queryset = Transaction.objects.all()
     serializer_class = TransactionSerializer
 
+
 class TransactionDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Transaction.objects.all()
     serializer_class = TransactionSerializer
-
 
 
 class AirtimeTransactionList(generics.ListCreateAPIView):
     queryset = AirtimeTransaction.objects.all()
     serializer_class = AirtimeTransactionSerializer
 
+
 class AirtimeTransactionDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = AirtimeTransaction.objects.all()
     serializer_class = AirtimeTransactionSerializer
 
+
 class ZesaTransactionsList(generics.ListCreateAPIView):
     queryset = ZesaTransactions.objects.all()
     serializer_class = ZesaTransactionSerializer
+
 
 class ZesaTransactionDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = ZesaTransactions.objects.all()
